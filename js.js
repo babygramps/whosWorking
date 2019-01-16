340982ce8fdd2fbbda69dab8969f2420006eda18
babygramps <pocolypz@gmail.com>
2019-01-16 12:55:39 -0800

    First upload

---
+++ js.js
@@ -0,0 +1,23 @@
+var stef = document.querySelector("#stef");
+var rick = document.querySelector("#rick");
+var levi = document.querySelector("#levi");
+
+var rickIMG = document.querySelector("#rickIMG");
+var leviIMG = document.querySelector("#leviIMG");
+
+
+var wasClicked = false;
+
+function greyClick(){
+    rickIMG.classList.toggle("clicked");
+}
+
+// rickIMG.addEventListener("click", function(){
+//     this.classList.toggle("clicked");
+// });
+
+// leviIMG.addEventListener("click", function(){
+//     this.classList.toggle("clicked");
+// });
+
+
