10fef7ffa49ee42c397638bea35d4c694544c303
babygramps <pocolypz@gmail.com>
2019-01-17 13:46:10 -0600

    From work done at home
    This changes my intent to have separate buttons toggle cards on and off

--- js.js
+++ js.js
@@ -1,23 +1,38 @@
-var stef = document.querySelector("#stef");
+var danny = document.querySelector("#danny");
 var rick = document.querySelector("#rick");
 var levi = document.querySelector("#levi");
 
-var rickIMG = document.querySelector("#rickIMG");
-var leviIMG = document.querySelector("#leviIMG");
+var rickCard = document.getElementById("rickCard");
+var dannyCard = document.getElementById("dannyCard");
+var leviCard = document.getElementById("leviCard");
 
-
-var wasClicked = false;
-
-function greyClick(){
-    rickIMG.classList.toggle("clicked");
-}
-
-// rickIMG.addEventListener("click", function(){
-//     this.classList.toggle("clicked");
+// staff.addEventListener("click", function (){
+//     for (var i = 0; i < staff.length; i++){
+//         var a = staff[i].classList.contains("clicked");
+//         if(a){
+//             staff[i].classList.remove("clicked");
+//         } else {
+//             staff[i].classList.add("clicked");
+//         }
+//     }
 // });
-
-// leviIMG.addEventListener("click", function(){
-//     this.classList.toggle("clicked");
-// });
+   
 
 
+rick.addEventListener("click", function(){
+    rickCard.classList.toggle("d-none");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
+
+danny.addEventListener("click", function(){
+    dannyCard.classList.toggle("d-none");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
+
+levi.addEventListener("click", function(){
+    leviCard.classList.toggle("d-none");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
