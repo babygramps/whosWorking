520198c20db3a09e48151e5a44048685a2c1ff7e
babygramps <pocolypz@gmail.com>
2019-01-17 15:39:10 -0600

    Add files via upload
    oops. uploaded the wrong folder last. these are the correct files

--- js.js
+++ js.js
@@ -1,129 +1,62 @@
-var numSquares = 6;
-var colors = [];
-var squares = document.querySelectorAll(".square");
-var pickedColor;
+var danny = document.querySelector("#danny");
+var rick = document.querySelector("#rick");
+var levi = document.querySelector("#levi");
+var anthony = document.querySelector("#anthony");
+var stef = document.querySelector("#stef");
+var avi = document.querySelector("#avi");
 
-var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelector("#message");
-var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset");
-var modeButtons = document.querySelectorAll(".modeBtn");
+var rickCard = document.getElementById("rickCard");
+var dannyCard = document.getElementById("dannyCard");
+var leviCard = document.getElementById("leviCard");
+var AnthonyCard = document.getElementById("anthonyCard");
+var StefCard = document.getElementById("stefCard");
+var AviCard = document.getElementById("aviCard");
 
-init();
+// staff.addEventListener("click", function (){
+//     for (var i = 0; i < staff.length; i++){
+//         var a = staff[i].classList.contains("clicked");
+//         if(a){
+//             staff[i].classList.remove("clicked");
+//         } else {
+//             staff[i].classList.add("clicked");
+//         }
+//     }
+// });
+   
 
-function init(){
-    setUpModeButtons();
-    setUpSquares();
-    reset();
-}
 
-// set up squares
-function setUpSquares(){
-    for(var i = 0; i < squares.length; i++) {
-        // add click listeners to squares
-            squares[i].addEventListener("click", function(){
-        // grab color of clicked square
-            var clickedColor = this.style.backgroundColor;
-        // compare color to pickedColor
-            if(clickedColor === pickedColor){
-                messageDisplay.textContent = "correct";
-                changeColors(clickedColor);
-                h1.style.backgroundColor = clickedColor;
-                resetButton.textContent = "Play again";
-            } else{
-                this.style.backgroundColor = "#232323";
-                messageDisplay.textContent = "try again";
-            }
-            });
-        }
-}
-
-// mode button event listeners
-function setUpModeButtons(){
-    for(var i = 0; i < modeButtons.length; i++){
-        modeButtons[i].addEventListener("click", function(){
-            // modeButtons[i].classList.remove("selected");
-            modeButtons[0].classList.remove("selected");
-            modeButtons[1].classList.remove("selected");
-            modeButtons[2].classList.remove("selected");
-            this.classList.add("selected");
-            if(this.textContent === "Easy"){
-                numSquares = 3;
-            } else if(this.textContent === "Hard") {
-                numSquares = 6;
-            } else {
-                numSquares = 9;
-            }
-            reset();
-        });
-    }
-}
-
-// reset button logic
-function reset(){
-        // generate new random colors
-            colors = generateRandomColors(numSquares);
-        // pick a new random color from arr
-            pickedColor = pickColor();
-        // change color display to match pickedColor
-            colorDisplay.textContent = pickedColor;
-        // reset message display
-            messageDisplay.textContent = "";
-            resetButton.textContent = "new colors"    
-        // change color of squares
-            for (var i = 0; i < squares.length; i++){
-                if(colors[i]){
-                    squares[i].style.display = "block";
-                    squares[i].style.background = colors[i];
-                } else{
-                    squares[i].style.display = "none";
-                }
-            }
-        // reset h1 background color
-            h1.style.backgroundColor = "steelblue";
-}
-
-// assign reset button logic to reset button
-resetButton.addEventListener("click", function(){
-    reset();
+rick.addEventListener("click", function(){
+    rickCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
 });
 
-// apply new colors to squares
-function changeColors(color) {
-// loop through all squares
-    for (var i = 0; i < squares.length; i++) {
-// change each color to match given color
-    squares[i].style.backgroundColor = color;
-    }
-}
+danny.addEventListener("click", function(){
+    dannyCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
 
-// assign winning color to var colors
-function pickColor() {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
-}
+levi.addEventListener("click", function(){
+    leviCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
 
-// generate array of random rgb colors
-function generateRandomColors(num) {
-// make an array
-    var arr = [];
-// add num random colors to array
-    for(var i = 0; i < num; i++) {
-        // get random color and push into arr
-        arr.push(randomColor())
-    }
-// return that array
-    return arr;
-}
+anthony.addEventListener("click", function(){
+    AnthonyCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
 
-// generate random values for vars r g and b
-function randomColor () {
-// pick a red 0 - 255
-    var r = Math.floor( Math.random() * 256);
-// pick a blue 0 - 255
-    var b = Math.floor( Math.random() * 256);
-// pick a green 0 - 255
-    var g = Math.floor( Math.random() * 256);
+stef.addEventListener("click", function(){
+    StefCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
 
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+avi.addEventListener("click", function(){
+    AviCard.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
+});
