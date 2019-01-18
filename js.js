a8e729dd8de97a3475b6fa14c7b2d3bf8b989218
Alessandro Voto <alex.voto@gmail.com>
2019-01-17 22:42:52 -0800

    trytry

--- js.js
+++ js.js
@@ -3,7 +3,9 @@ var staff = ["rick", "danny", "levi", "anthony", "stef", "avi"];
 for (var i=0; i<staff.length; i++){
     var idSelected = document.querySelector("#"+staff[i]);
     var cardSelected = document.getElementById(staff[i] + "Card");
-    
+}
+
+for (var i=0; i<staff.length; i++)
     idSelected.addEventListener("click", function(){
     cardSelected.classList.toggle("clicked");
     idSelected.classList.toggle("btn-light");
