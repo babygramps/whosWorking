573d6c54e91c2c143bd3e9e8d89ce9222f4b9526
Alessandro Voto <alex.voto@gmail.com>
2019-01-17 22:25:55 -0800

    Refactor: Adding missing DOM selections

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 var staff = ["rick", "danny", "levi", "anthony", "stef", "avi"];
 
 for (var i=0; i<staff.length; i++){
-    staff[i].addEventListener("click", function(){
-        cardified = staff[i] + "Card"; // concatenates the word "Card" at the end of the staff name (a string) from the staff array
-        cardified.classList.toggle("clicked");
-        this.classList.toggle("btn-light");
-        this.classList.toggle("clicked");
+    var idSelected = document.querySelector("#"+staff[i]);
+    var cardSelected = document.getElementByID(staff[i] + "Card");
+    
+    idSelected.addEventListener("click", function(){
+    cardSelected.classList.toggle("clicked");
+    this.classList.toggle("btn-light");
+    this.classList.toggle("clicked");
     });
 }
