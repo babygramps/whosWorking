28041b5b430e0a489bb8b532656a51299f7432fc
Alessandro Voto <alex.voto@gmail.com>
2019-01-17 22:37:13 -0800

    Another try
    *sigh*

--- js.js
+++ js.js
@@ -2,11 +2,11 @@ var staff = ["rick", "danny", "levi", "anthony", "stef", "avi"];
 
 for (var i=0; i<staff.length; i++){
     var idSelected = document.querySelector("#"+staff[i]);
-    var cardSelected = document.getElementByID(staff[i] + "Card");
+    var cardSelected = document.getElementById(staff[i] + "Card");
     
     idSelected.addEventListener("click", function(){
     cardSelected.classList.toggle("clicked");
-    this.classList.toggle("btn-light");
-    this.classList.toggle("clicked");
+    idSelected.classList.toggle("btn-light");
+    idSelected.classList.toggle("clicked");
     });
 }
